f50cf73a6ff4b29bddac8d9c7607094548f2158e
Geun Tae Kim <lkes21@naver.com>
2023-04-26 23:53:58 +0900

    first commit4

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositorytest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositorytest.java
@@ -2,14 +2,22 @@ package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class MemoryMemberRepositorytest {
-    MemberRepository repository = new MemoryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    // 하나의 테스트가 끝날때 마다 초기화 시켜준다. clear 사용
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
 
     @Test
     public void save(){
@@ -40,6 +48,22 @@ public class MemoryMemberRepositorytest {
 
         Member result = repository.findByName("spring1").get();
         assertThat(result).isEqualTo(member1);
+
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
 
     }
 }
